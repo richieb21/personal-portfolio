32b2306f3fe6eb77a4b2a8b288c4195d63e0f368
richieb21 <76607899+richieb21@users.noreply.github.com>
2024-09-13 18:43:02 -0400

    Escaped Character

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -8,34 +8,15 @@ import { FlipWords } from "@/components/ui/flip-words";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function HeroSection() {
-  const [buttonText, setButtonText] = useState("Seeking 2024 Summer COOP! 🚀");
-  const [isFading, setIsFading] = useState(false);
-
   const words = ["a Software Engineer", "a Problem Solver", "an Innovator"];
-
-  const handleChangeText = () => {
-    setIsFading(true);
-    setTimeout(() => {
-      setButtonText((prevButtonText) =>
-        prevButtonText === "Seeking 2025 Winter Co-ops 🚀"
-          ? "Take a look at my resume!"
-          : "Seeking 2025 Winter Co-ops 🚀"
-      );
-      setIsFading(false);
-    }, 250);
-  };
-
-  const handlePDF = () => {
-    window.open("/resume.pdf", "_blank");
-  };
 
   return (
     <div className="min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between">
       <div className="space-y-6 text-center lg:text-left">
         <h1 className="text-4xl font-normal lg:text-5xl">
-          Hi, I'm Richard Bai 👋 <br />{" "}
+          Hi, I&apos;m Richard Bai 👋 <br />{" "}
           <span className="mt-2 block">
-            {"And I'm"}
+            {"And Im"}
             <FlipWords words={words} />
           </span>
         </h1>
