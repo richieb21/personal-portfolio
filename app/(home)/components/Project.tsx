26b34d135be194bc55ff98ac8cfd28e12f5fb05f
richieb21 <76607899+richieb21@users.noreply.github.com>
2024-12-05 02:01:50 -0500

    Project overhaul

--- app/(home)/components/Project.tsx
+++ app/(home)/components/Project.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import {
+  SiD3Dotjs,
   SiExpress,
+  SiFlask,
   SiMongodb,
   SiNextdotjs,
   SiNodedotjs,
+  SiOpenai,
   SiPostman,
   SiReact,
   SiRender,
@@ -20,31 +23,39 @@ import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 export default function Project() {
   const projects = [
     {
-      title: "Code Wizard",
-      tech: [SiReact, SiNodedotjs, SiExpress, SiMongodb, SiPostman],
-      link: "https://github.com/richieb21/code-wizard",
-      cover: "/code-wizard.png",
+      title: "elitecode",
+      description:
+        "A LeetCode-inspired platform featuring interactive coding challenges with real-time code execution and a built-in compiler.",
+      tech: [SiReact, SiD3Dotjs, SiNodedotjs, SiExpress, SiMongodb],
+      link: "https://github.com/elitecode-org",
+      cover: "/elitecode.png",
       background: "bg-white",
     },
     {
-      title: "Beacons.us",
-      tech: [SiReact, SiNodedotjs, SiExpress, SiMongodb, SiPostman],
-      link: "https://github.com/willi-li-am/beacons",
-      cover: "/beacons.png",
+      title: "ChatIsThisReal",
+      description:
+        "A chrome extension that communicates to an AI agent in real time through audio and textual transcription to provide users with due diligence",
+      tech: [SiReact, SiFlask, SiOpenai, SiMongodb, SiPostman],
+      link: "https://github.com/richieb21/opensesame",
+      cover: "/chatisthisreal.png",
+      background: "bg-white",
+    },
+    {
+      title: "Acceleratr",
+      description:
+        "A resume builder that uses BERT to match best suited projects and experiences from a master resume to automatically generate and compile a latex resume",
+      tech: [SiVite, SiNodedotjs, SiExpress, SiMongodb, SiSocketdotio],
+      link: "https://github.com/richieb21/ctrl-hack-del",
+      cover: "/acceleratr.png",
       background: "bg-white",
     },
     {
       title: "Sorting Visualizer",
+      description:
+        "An interactive visualization tool demonstrating various sorting algorithms in action, helping users understand sorting mechanics.",
       tech: [SiNextdotjs, SiVercel, SiTypescript],
       link: "https://sorting-visualizer-88s3.vercel.app/",
       cover: "/sorting.png",
-      background: "bg-white",
-    },
-    {
-      title: "Pulsar Chat-App",
-      tech: [SiVite, SiNodedotjs, SiExpress, SiMongodb, SiSocketdotio],
-      link: "https://github.com/richieb21/pulsar-chatapp",
-      cover: "/pulsar.png",
       background: "bg-white",
     },
   ];
@@ -56,7 +67,7 @@ export default function Project() {
         className="flex flex-col items-center justify-center"
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
+      <div className="grid grid-cols-2 pt-10 gap-8">
         {projects.map((project, index) => {
           return (
             <Link
@@ -65,21 +76,30 @@ export default function Project() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className={cn("p-0.5 rounded-lg ", project.background)}>
-                <DirectionAwareHover
-                  imageUrl={project.cover}
-                  className="w-full space-y-5"
-                >
-                  <div className="space-y-5">
-                    <h1 className="text-2xl font-bold">{project.title}</h1>
-
-                    <div className="flex items-center gap-5">
-                      {project.tech.map((Icon, idx) => {
-                        return <Icon key={idx} className="w-8 h-8" />;
-                      })}
+              <div
+                className={cn(
+                  "rounded-lg p-[1px] bg-gradient-to-r from-pink-500 to-purple-500",
+                  project.background
+                )}
+              >
+                <div className="bg-black rounded-lg">
+                  <DirectionAwareHover
+                    imageUrl={project.cover}
+                    className="w-full space-y-5"
+                  >
+                    <div className="space-y-5">
+                      <h1 className="text-2xl font-bold">{project.title}</h1>
+                      <p className="text-sm text-neutral-300">
+                        {project.description}
+                      </p>
+                      <div className="flex items-center gap-5">
+                        {project.tech.map((Icon, idx) => {
+                          return <Icon key={idx} className="w-8 h-8" />;
+                        })}
+                      </div>
                     </div>
-                  </div>
-                </DirectionAwareHover>
+                  </DirectionAwareHover>
+                </div>
               </div>
             </Link>
           );
